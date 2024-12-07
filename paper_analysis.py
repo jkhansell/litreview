937af2e215496db05057d1c7ec5341c194a23331
Johansell Villalobos <johansellvilla@gmail.com>
2024-12-06 22:38:15 -0600

    Updated repo with new keywords and subroutines

--- paper_analysis.py
+++ paper_analysis.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 import re
+
 plt.rcParams['axes.axisbelow'] = True
 
 from datasets import Dataset
@@ -17,44 +18,20 @@ def unify_databases(sources):
     
     df = pd.concat(source_dfs, axis=0)
     df = df.drop(df[df["Publication Year"] < 2000].index)
+    df = df.drop(df[df["Publication Year"] > 2024].index)
     df.reset_index(inplace=True)
 
     return df
 
 def normalize_text(text):
     """Normalize text for comparison by removing punctuation, lowercasing, and trimming whitespace."""
-    return re.sub(r'\W+', '', text.strip())
+    return re.sub(r'\W+', '', text.lower().strip())
 
 def clean_dataframe(df):
     df["TitleNorm"] = df["Title"].map(normalize_text)
     df = df.drop_duplicates(subset=["TitleNorm"], keep="last")
     df.reset_index(inplace=True)
     return df
-
-def gensim_tokenize_data(df):
-    df["Abstract Note"] = df["Abstract Note"].map(str)
-    df["gensim_tokenized_titles"] = df["Title"].map(lambda x: simple_preprocess(x.lower(), deacc=True))
-    df["gensim_tokenized_abstracts"] = df["Abstract Note"].map(lambda x: simple_preprocess(x.lower(), deacc=True))
-    return df
-
-
-def graph_distribution(df, name=None):
-    plt.hist(df["Publication Year"],bins=9)
-    plt.grid(alpha=0.5)
-    plt.xlabel("Year")
-    plt.ylabel("Paper count")
-    plt.xticks(np.arange(2007,2025,2))
-    plt.savefig("yeardistribution"+name+".png", dpi=130)
-    plt.close()
-
-def graph_sources(df, name=None):
-    sources_df = df.groupby("Source").count()["Title"]
-    plt.figure(figsize=(11,5))
-    ax = sources_df.plot(kind="barh")
-    ax.set_xlabel("Paper counts")
-    plt.grid(alpha=0.5)
-    plt.savefig("barplot"+name+".png")
-    plt.close()
 
     # Function to process abstracts in batches
 def process_batch(batch, labels, classifier):
@@ -69,105 +46,51 @@ def process_batch(batch, labels, classifier):
 if __name__ == "__main__":
     sources = ["Google-Scholar", "Web-of-Science", "IEEE", "Scopus"]
     df = unify_databases(sources)
-    #graph_sources(df, name="_raw")
-    #graph_distribution(df, name="_raw")
     # clean data
     df = clean_dataframe(df)
-    #graph_distribution(df, name="_processed")
-    #graph_sources(df, name="_processed")
 
-    # classify papers using NLP
-    classifier = pipeline("zero-shot-classification", model="MoritzLaurer/deberta-v3-large-zeroshot-v2.0", device=0, torch_dtype="auto")
-    labels = [
-        ["bed load transport"],
-        ["high performance computing enabled"],
-        ["two dimensional"],
-        ["finite Volume method"],
-        ["finite element method"],
-        ["finite difference method"],
-        ["numerical method development"],
-        ["riverbed morphology"],
-        ["general hydrological modeling"], 
-        ["ocean modeling"], 
-        ["presents computational performance reports"],
-        ["performs validation of numerical solution"]
+    models = [
+        "facebook/bart-large-mnli", 
+        "MoritzLaurer/deberta-v3-large-zeroshot-v2.0"
     ]
+    
+    for model in models:
+        modelstr = model.split("/")[1]
+        # classify papers using NLP
+        classifier = pipeline("zero-shot-classification", model=model, device=0, torch_dtype="auto")
+        labels = [
+            ["bedload transport"],
+            ["mobile bed"],
+            ["high performance computing enabled"],
+            ["gpu accelerated"],
+            ["distributed memory"],
+            ["cpu multithreading"],
+            ["two dimensional"],
+            ["one dimensional"],
+            ["deterministic"],
+            ["stochastic"],
+            ["finite volume method"],
+            ["finite element method"],
+            ["finite difference method"],
+            ["numerical method development"],
+            ["riverbed morphology"],
+            ["hydrological modeling"], 
+            ["ocean modeling"], 
+            ["computational performance report"],
+            ["validation of numerical method"],
+            ["iber"],
+            ["telemac"],
+            ["hec-ras"]
+        ]
 
-    for label in labels:
-        dataset = Dataset.from_pandas(df[['Abstract Note']])
+        
 
-        # Apply batch processing
-        # Results are better when it's binary processing instead of Multi-Label classification
-        batch_size = 64  # You can adjust the batch size based on available GPU memory
-        results = dataset.map(lambda x: process_batch(x, label, classifier), batched=True, batch_size=batch_size)
-        # Convert the results back to a pandas DataFrame for easier analysis
-        result_df = pd.DataFrame(results)        
-        df[label[0]] = result_df["scores"]
+        for label in labels:
+            dataset = Dataset.from_pandas(df[['Abstract Note']])
+            results = dataset.map(lambda x: process_batch(x, label, classifier), batched=True, batch_size=64)
+            result_df = pd.DataFrame(results)
+            # Flatten scores into the DataFrame and convert to float
+            df[label[0]+"_"+modelstr] = result_df['scores'].map(lambda x: x if isinstance(x, float) else float(x[0]))
 
-    df.to_csv("classified.csv")
-    # Display the processed results
-
-    df["BLT"] = df["bed load transport"].map(lambda x: x.replace("[", "").replace("]", "")).astype(float) > 0.85
-    df["HPC"] = df["high performance computing enabled"].map(lambda x: x.replace("[", "").replace("]", "")).astype(float) > 0.85
-    df["2D"] = df["two dimensional"].map(lambda x: x.replace("[", "").replace("]", "")).astype(float) > 0.85
-    df["FVM"] = df["finite Volume method"].map(lambda x: x.replace("[", "").replace("]", "")).astype(float) > 0.85
-    df["FEM"] = df["finite element method"].map(lambda x: x.replace("[", "").replace("]", "")).astype(float) > 0.85
-    df["FDM"] = df["finite difference method"].map(lambda x: x.replace("[", "").replace("]", "")).astype(float) > 0.85
-    df["NMD"] = df["numerical method development"].map(lambda x: x.replace("[", "").replace("]", "")).astype(float) > 0.85
-    df["CPR"] = df["presents computational performance reports"].map(lambda x: x.replace("[", "").replace("]", "")).astype(float) > 0.85
-    df["VNS"] = df["performs validation of numerical solution"].map(lambda x: x.replace("[", "").replace("]", "")).astype(float) > 0.85
-
-    labels = ["BLT", "HPC", "2D", "FVM", "FEM", "FDM", "NMD", "CPR", "VNS"]
-    custom_colors = ['#ff9999', '#66b3ff', '#99ff99']
-
-    fig, ax = plt.subplots()
-
-    trues = []
-    falses = []
-    values = None
-    for label in labels:
-        classes, values = np.unique(df[label], return_counts=True)
-        trues.append(100*values[1]/np.sum(values))
-
-    z = [x for _, x in sorted(zip(trues, labels), reverse=True)]
-
-    #plt.bar(labels, falses, color=custom_colors[0])
-    plt.bar(z, sorted(trues, reverse=True), color=custom_colors[1])
-    plt.grid()
-    plt.xlabel("Topic Abbreviation")
-    plt.ylabel("Percentage of papers")
-    plt.text(5.5, 60, "Total papers: "+str(np.sum(values)), size=10,
-            ha="center", va="center",
-            bbox=dict(boxstyle="round",
-                    ec=(0., 0.5, 0.5),
-                    fc=(1., 1, 1),
-                    )
-            )
-    plt.savefig("overall.png")
-    plt.close()
-
-    """    fig = go.Figure()
-    fig.add_trace(go.Scatterpolar(
-        r=trues, 
-        theta=labels,
-        name="Total papers: 92"
-    ))
-    fig.update_layout(
-                polar=dict(
-                    radialaxis=dict(
-                    visible=True
-                    ),
-                ),
-                font=dict(
-                    size=18,  # Set the font size here
-                ),
-                legend={
-                    "x": 0.3,
-                    "xref": "container",
-                    "yref": "container",
-                    "orientation": "h"
-                },
-                showlegend=True
-            )
-    fig.write_image("radar_performance.png",width=800, height=600, scale=1.5)
-    """
+        df.to_csv("classified.csv")
+        # Display the processed results
--- zeroshot.py
+++ zeroshot.py
@@ -1,71 +1,150 @@
 import pandas as pd
+import matplotlib.pyplot as plt 
 import numpy as np
-import plotly.graph_objects as go
-import matplotlib.pyplot as plt
+
 plt.rcParams['axes.axisbelow'] = True
-
-
-df = pd.read_csv("classified.csv")
-
-df["BLT"] = df["bed load transport"].map(lambda x: x.replace("[", "").replace("]", "")).astype(float) > 0.85
-df["HPC"] = df["high performance computing enabled"].map(lambda x: x.replace("[", "").replace("]", "")).astype(float) > 0.85
-df["2D"] = df["two dimensional"].map(lambda x: x.replace("[", "").replace("]", "")).astype(float) > 0.85
-df["FVM"] = df["finite Volume method"].map(lambda x: x.replace("[", "").replace("]", "")).astype(float) > 0.85
-df["FEM"] = df["finite element method"].map(lambda x: x.replace("[", "").replace("]", "")).astype(float) > 0.85
-df["FDM"] = df["finite difference method"].map(lambda x: x.replace("[", "").replace("]", "")).astype(float) > 0.85
-df["NM"] = df["numerical method development"].map(lambda x: x.replace("[", "").replace("]", "")).astype(float) > 0.85
-
-labels = ["BLT","HPC", "2D", "FVM", "FEM", "FDM", "NM"]
 custom_colors = ['#ff9999', '#66b3ff', '#99ff99']
 
-fig, ax = plt.subplots()
+def graph_distribution(df):
+    plt.figure(figsize=(6,4))
+    plt.hist(df["Publication Year"],bins=8)
+    plt.grid(alpha=0.5)
+    plt.xlabel("Year")
+    plt.ylabel("Paper count")
+    plt.savefig("yeardistribution_processed.png", dpi=130)
+    plt.close()
 
-trues = []
-falses = []
-values = None
-for label in labels:
-    classes, values = np.unique(df[label], return_counts=True)
-    trues.append(100*values[1]/np.sum(values))
-    falses.append(100*values[0]/np.sum(values))
+def graph_sources(df):
+    sources_df = df.groupby("Source").count()["Title"]
+    plt.figure(figsize=(12,4))
+    ax = sources_df.plot(kind="barh")
+    ax.set_xlabel("Paper counts")
+    plt.grid(alpha=0.5)
+    plt.savefig("barplot_processed.png")
+    plt.close()
 
-z = [x for _, x in sorted(zip(trues, labels), reverse=True)]
+def plot_class_distribution(df, models, labels):
+    for model in models:
 
-#plt.bar(labels, falses, color=custom_colors[0])
-plt.bar(z, sorted(trues, reverse=True), color=custom_colors[1])
-plt.grid()
-plt.xlabel("Topic Abbreviation")
-plt.ylabel("Percentage of papers")
-plt.text(5.5, 60, "Total papers: "+str(np.sum(values)), size=10,
-         ha="center", va="center",
-         bbox=dict(boxstyle="round",
-                   ec=(0., 0.5, 0.5),
-                   fc=(1., 1, 1),
-                   )
-         )
-plt.savefig("overall.png")
-plt.close()
+        df["BLT"+"_"+model] = df["bedload transport"+"_"+model].astype(float) > 0.875
+        df["MB"+"_"+model] = df["mobile bed"+"_"+model].astype(float) > 0.875
+        df["HPC"+"_"+model] = df["high performance computing enabled"+"_"+model].astype(float) > 0.875
+        df["GPU"+"_"+model] = df["gpu accelerated"+"_"+model].astype(float) > 0.875
+        df["CPU"+"_"+model] = df["cpu multithreading"+"_"+model].astype(float) > 0.875
+        df["MPI"+"_"+model] = df["distributed memory"+"_"+model].astype(float) > 0.875
+        df["2D"+"_"+model] = df["two dimensional"+"_"+model].astype(float) > 0.875
+        df["1D"+"_"+model] = df["one dimensional"+"_"+model].astype(float) > 0.875
+        df["DET"+"_"+model] = df["deterministic"+"_"+model].astype(float) > 0.875
+        df["STC"+"_"+model] = df["stochastic"+"_"+model].astype(float) > 0.875
+        df["FVM"+"_"+model] = df["finite volume method"+"_"+model].astype(float) > 0.875
+        df["FEM"+"_"+model] = df["finite element method"+"_"+model].astype(float) > 0.875
+        df["FDM"+"_"+model] = df["finite difference method"+"_"+model].astype(float) > 0.875
+        df["NMD"+"_"+model] = df["numerical method development"+"_"+model].astype(float) > 0.875
+        df["RBM"+"_"+model] = df["riverbed morphology"+"_"+model].astype(float) > 0.875
+        df["GHM"+"_"+model] = df["hydrological modeling"+"_"+model].astype(float) > 0.875
+        df["OM"+"_"+model] = df["ocean modeling"+"_"+model].astype(float) > 0.875
+        df["CPR"+"_"+model] = df["computational performance report"+"_"+model].astype(float) > 0.875
+        df["VNM"+"_"+model] = df["validation of numerical method"+"_"+model].astype(float) > 0.875
+        df["IBER"+"_"+model] = df["iber"+"_"+model].astype(float) > 0.875
+        df["TELEMAC"+"_"+model] = df["telemac"+"_"+model].astype(float) > 0.875
+        df["HEC-RAS"+"_"+model] = df["hec-ras"+"_"+model].astype(float) > 0.875
 
-fig = go.Figure()
-fig.add_trace(go.Scatterpolar(
-    r=trues, 
-    theta=labels,
-    name="Total papers: 92"
-))
-fig.update_layout(
-            polar=dict(
-                radialaxis=dict(
-                visible=True
-                ),
-            ),
-            font=dict(
-                size=18,  # Set the font size here
-            ),
-            legend={
-                "x": 0.3,
-                "xref": "container",
-                "yref": "container",
-                "orientation": "h"
-            },
-            showlegend=True
-        )
-fig.write_image("radar_performance.png",width=800, height=600, scale=1.5)
+        fig, ax = plt.subplots(figsize=(14,6))
+
+        trues = []
+        values = None
+
+        for label in labels:
+            classes, values = np.unique(df[label+"_"+model], return_counts=True)
+            trues.append(100*(len(df[label+"_"+model])-values[0])/len(df[label+"_"+model]))
+
+        z = [x for _, x in sorted(zip(trues, labels), reverse=True)]
+
+        #plt.bar(labels, falses, color=custom_colors[0])
+        plt.figure(figsize=(16,4))
+        plt.bar(z, sorted(trues, reverse=True), color=custom_colors[1])
+        for i in range(len(labels)):
+            plt.text(z[i], sorted(trues, reverse=True)[i]/2, "{:02.1f}".format(sorted(trues, reverse=True)[i]), ha="center")
+        plt.grid()
+        plt.xlabel("Topic Abbreviation")
+        plt.ylabel("Percentage of papers")
+        plt.text(0.9, 0.9, "Total papers: "+str(np.sum(values)), size=10, transform=plt.gca().transAxes,
+                ha="center", va="center",
+                bbox=dict(boxstyle="round",
+                        ec=(0., 0.5, 0.5),
+                        fc=(1., 1, 1),
+                        )
+                )
+        plt.savefig("overall_"+model+".png")
+        plt.close()
+
+    return df
+
+def get_overall_classes(df, models, labels):
+    
+    trues = []
+    values = None
+    for label in labels:
+        # Get classes of both models
+        # Select columns for the current label
+        cols = ["{}_{}".format(label, model) for model in models]
+        
+        df[label] = df[cols].any(axis=1)
+        #df[df[label]]["Title"].to_csv(label+".csv")
+
+        classes, values = np.unique(df[label], return_counts=True)
+        trues.append(100*(len(df[label])-values[0])/len(df[label]))
+
+    z = [x for _, x in sorted(zip(trues, labels), reverse=True)]
+
+    plt.figure(figsize=(16,4))
+    plt.bar(z, sorted(trues, reverse=True), color=custom_colors[1])
+    for i in range(len(labels)):
+        plt.text(z[i], sorted(trues, reverse=True)[i]/2, "{:02.1f}".format(sorted(trues, reverse=True)[i]), ha="center")
+    plt.grid()
+    plt.xlabel("Topic Abbreviation")
+    plt.ylabel("Percentage of papers")
+    plt.text(0.9, 0.9, "Total papers: "+str(np.sum(values)), size=10, transform=plt.gca().transAxes,
+            ha="center", va="center",
+            bbox=dict(boxstyle="round",
+                    ec=(0., 0.5, 0.5),
+                    fc=(1., 1, 1),
+                    )
+            )
+
+    plt.savefig("overall.png")
+    plt.close()
+    
+    return df
+
+def filter_classes(df):
+    #considering all which regard numerical method development
+    df_NMD = df[df["NMD"]]
+    # we only want the ones that have to dow with 2 dimensional modeling
+    df_2D = df_NMD[df_NMD["2D"]]
+    # We only consider the ones that are not explicitly stochastic
+    df_2D = df_2D[np.logical_not(df_2D["STC"])]
+    # We will classify then the ones that are related to HPC and the ones that are not 
+    print(np.unique(df_2D["HPC"], return_counts=True))
+    
+    df_2D[["Title", "Abstract Note"]].to_csv("2D_read.csv")
+    df_2D[np.logical_not(df_2D["HPC"])][["Title", "Abstract Note"]].to_csv("notHPC.csv")
+    df_2D[df_2D["HPC"]][["Title", "Abstract Note"]].to_csv("2DHPC.csv")
+
+
+if __name__ == "__main__":
+
+    df = pd.read_csv("classified.csv")
+    graph_sources(df)
+    graph_distribution(df)
+
+    models = [
+        "bart-large-mnli", 
+        "deberta-v3-large-zeroshot-v2.0"
+    ]
+
+    labels = ["BLT", "MB", "HPC", "GPU", "CPU", "MPI", "2D", "1D", "DET", "STC", "FVM", "FEM", "FDM", "NMD", "RBM", "GHM", "OM", "CPR", "VNM", "IBER", "TELEMAC", "HEC-RAS"]
+
+    df = plot_class_distribution(df, models, labels)
+    df = get_overall_classes(df, models, labels)
+    df = filter_classes(df)
